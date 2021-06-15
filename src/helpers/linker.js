b095162c39798b7345aac66b1ae795e752ca485b
wipaaa <ini.wipaaa@gmail.com>
2021-06-15 21:48:11 +0800

    Add linker helper linker helper is help us to connect to the provider and wallet

---
+++ src/helpers/linker.js
@@ -0,0 +1,37 @@
+const {
+  providers: { JsonRpcProvider },
+  Wallet,
+} = require('ethers');
+const constants = require('../configs/constants');
+
+module.exports = class Linker {
+  #_provider = null;
+  #_wallet = null;
+  #_options = {
+    privateKey: constants.accountKey,
+    providerOrUrl: constants.uriRemoteProvider,
+  };
+
+  async connect(options = {}) {
+    this.#_mergeOptions(options);
+
+    this.#_provider = new JsonRpcProvider(this.#_options.providerOrUrl);
+    await this.#_provider.ready; // await to provider to be connected
+    this.#_wallet = new Wallet(this.#_options.privateKey, this.#_provider);
+  }
+
+  #_mergeOptions(options) {
+    this.#_options = {
+      ...this.#_options,
+      ...options,
+    };
+  }
+
+  get provider() {
+    return this.#_provider;
+  }
+
+  get wallet() {
+    return this.#_wallet;
+  }
+};
